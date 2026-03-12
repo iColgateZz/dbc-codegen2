8e0b53a5e2c0e62e99d679c3b4687abceffc1277
pavelpavliv <ppavli@taltech.ee>
2026-03-12 18:17:59 +0200

    rm 2 sanitizers

--- src/middle_end/nodes/mod.rs
+++ src/middle_end/nodes/mod.rs
@@ -1,14 +1,9 @@
-pub mod helpers;
 pub mod message_name_sanitizer;
 pub mod signal_enum_sanitizer;
 pub mod signal_value_enum_attacher;
 pub mod transformation;
-pub mod signal_name_sanitizer;
-pub mod signal_enum_signal_name_sanitizer;
 
 pub use message_name_sanitizer::*;
 pub use signal_enum_sanitizer::*;
 pub use signal_value_enum_attacher::*;
 pub use transformation::TransformationNode;
-pub use signal_name_sanitizer::*;
-pub use signal_enum_signal_name_sanitizer::*;
