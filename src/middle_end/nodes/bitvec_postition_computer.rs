54236ddb4563a4813c384a9657c71fe893b28a64
David Bongartz <d.bongartz@gmail.com>
2026-08-18 15:57:08 +0200

    Simplify the BigEndian (start, end) calculation

--- src/middle_end/nodes/bitvec_postition_computer.rs
+++ src/middle_end/nodes/bitvec_postition_computer.rs
@@ -2,7 +2,7 @@ use super::transformation::TransformationNode;
 use crate::ir::signal_layout::ByteOrder;
 
 /// Pre-compute bitvec slice positions for each signal layout.
-/// Translates DBC start_bit (which varies by byte order) into
+/// Translates DBC `start_bit` (which varies by byte order) into
 /// a unified [start..end] range suitable for bitvec indexing.
 pub struct ComputeBitvecPositions;
 
@@ -11,16 +11,16 @@ impl TransformationNode for ComputeBitvecPositions {
         for layout in &mut file.signal_layouts {
             let (start, end) = match layout.byte_order {
                 ByteOrder::LittleEndian => {
-                    let start = layout.start_bit as usize;
-                    let end = start + layout.size as usize;
+                    let start = usize::try_from(layout.start_bit).unwrap();
+                    let end = start + usize::try_from(layout.size).unwrap();
                     (start, end)
                 }
                 ByteOrder::BigEndian => {
-                    let start_bit = layout.start_bit;
-                    let x = (start_bit / 8) * 8;
-                    let y = 7 - (start_bit % 8);
-                    let start = (x + y) as usize;
-                    let end = start + layout.size as usize;
+                    // This ensures correct start-end bit positions for the
+                    // Motorola BigEndian "ZigZag" encoding when paired with
+                    // bitvec's `*<Msb0>::*_be(...)` functions.
+                    let start = usize::try_from(layout.start_bit).unwrap() ^ 7;
+                    let end = start + usize::try_from(layout.size).unwrap();
                     (start, end)
                 }
             };
