4555f5699e365bb25a57c132669f0749360c63e9
David Bongartz <d.bongartz@gmail.com>
2026-08-19 10:45:11 +0200

    Improve conversion error handling in ComputeBitvecPositions

--- src/middle_end/nodes/bitvec_postition_computer.rs
+++ src/middle_end/nodes/bitvec_postition_computer.rs
@@ -9,24 +9,21 @@ pub struct ComputeBitvecPositions;
 impl TransformationNode for ComputeBitvecPositions {
     fn transform(&self, file: &mut crate::DbcFile) {
         for layout in &mut file.signal_layouts {
-            let (start, end) = match layout.byte_order {
-                ByteOrder::LittleEndian => {
-                    let start = usize::try_from(layout.start_bit).unwrap();
-                    let end = start + usize::try_from(layout.size).unwrap();
-                    (start, end)
-                }
-                ByteOrder::BigEndian => {
-                    // This ensures correct start-end bit positions for the
-                    // Motorola BigEndian "ZigZag" encoding when paired with
-                    // bitvec's `*<Msb0>::*_be(...)` functions.
-                    let start = usize::try_from(layout.start_bit).unwrap() ^ 7;
-                    let end = start + usize::try_from(layout.size).unwrap();
-                    (start, end)
-                }
+            let (start, size) = (
+                usize::try_from(layout.start_bit).expect("signal start_bit exceeded usize::MAX"),
+                usize::try_from(layout.size).expect("signal size exceeded usize::MAX"),
+            );
+
+            let start_adjusted = match layout.byte_order {
+                ByteOrder::LittleEndian => start,
+                // This ensures correct start-end bit positions for the
+                // Motorola BigEndian "ZigZag" encoding when paired with
+                // bitvec's `*<Msb0>::*_be(...)` functions.
+                ByteOrder::BigEndian => start ^ 7,
             };
 
-            layout.bitvec_start = start;
-            layout.bitvec_end = end;
+            layout.bitvec_start = start_adjusted;
+            layout.bitvec_end = start_adjusted + size;
         }
     }
 }
